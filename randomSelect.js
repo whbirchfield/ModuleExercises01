81b673655f08f7e39d75e748b0ae607fc7ae8480
Will B <willhbjr@gmail.com>
2021-03-09 15:15:57 +0000

    Initial commit

---
+++ randomSelect.js
@@ -0,0 +1,7 @@
+ function randomSelection(arr){
+   let index = Math.floor(Math.random()*arr.length);
+   return arr[index];
+}
+
+//TODO: Export the randomFromArray function.
+module.export = randomSelection;
